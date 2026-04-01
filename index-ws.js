4dd0a755a28abf8f1a54a09dc3c5198bf95fcc4c
beingaashish <beingaashishr@gmail.com>
2026-04-01 08:29:14 +0545

    Created a websocket server and client

--- index-ws.js
+++ index-ws.js
@@ -11,3 +11,30 @@ server.on('request', app);
 server.listen(3000, function () {
   console.log('Server Started on port 3000');
 })
+
+/** Begin websocket */
+const webSocketServer = require('ws').Server;
+
+const wss = new webSocketServer({ server: server })
+
+wss.on('connection', function (ws) {
+  const numClients = wss.clients.size;
+  console.log('Clients Connected', numClients);
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState == ws.OPEN) {
+    ws.send('Welcome to my Server');
+  }
+
+  ws.on('close', function close() {
+    wss.broadcast(`Current visitors: ${numClients}`);
+    console.log('A client has disconnected');
+  });
+
+})
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client){
+    client.send(data);
+  })
+}
